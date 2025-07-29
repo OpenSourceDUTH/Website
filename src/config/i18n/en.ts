342929618f2a04f9634fbd56d050c0e6b84cd071
leokratis <leokratisbak@gmail.com>
2025-07-29 17:56:07 +0300

    Update: Still loosing my mind over translation techniques

---
+++ src/config/i18n/en.ts
@@ -0,0 +1,19 @@
+export default {
+  landing_page: {
+    announcement: "We are co-hosting GRCPC 2025!",
+    about_us: {
+      title: "About Us",
+      description: "We are a group of university students passionate about open source software and the FOSS community. Our goal is to create a space where students can learn and contribute to the open source community.",
+      list: {
+        collaborative: {
+          title: "Collaboration",
+          description: "In our team, we value collaboration and teamwork because together we can achieve great things."
+        },
+        innovative: {
+          title: "Innovation",
+          description: "We always seek to engage with the latest technologies and innovations and contribute to them."
+        }
+      }
+    }
+  },
+}
